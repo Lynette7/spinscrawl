826d49f5b0c707461a3b05b0fafc2965af6e8956
Lynette7 <lynettewanjiru7@gmail.com>
2023-03-25 23:14:41 +0300

    Updated code

--- main.js
+++ main.js
@@ -1,71 +1,41 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const fs = require('fs');
+const path = require('path');
+
 const url = "https://www.iban.com/exchange-rates";
 
-fetchData(url).then( (res) => {
-    const html = res.data;
-    const $ = cheerio.load(html);
-    const statsTable = $('.table.table-bordered.table-hover.downloads > tbody > tr');
-    statsTable.each(function() {
-        let title = $(this).find('td').text();
-        console.log(title);
-    });
-})
-
-async function fetchData(url){
-    console.log("Crawling data...")
-    // make http call to url
-    let response = await axios(url).catch((err) => console.log(err));
-
-    if(response.status !== 200){
-        console.log("Error occurred while fetching data");
-        return;
-    }
-    return response;
-}
-
-let workDir = __dirname+"/dbWorker.js";
-
-const mainFunc = async () => {
-  const url = "https://www.iban.com/exchange-rates";
-  // fetch html data from iban website
-  let res = await fetchData(url);
-  if(!res.data){
-    console.log("Invalid data Obj");
-    return;
-  }
+fetchData(url).then((res) => {
   const html = res.data;
-  let dataObj = new Object();
-  // mount html page to the root element
   const $ = cheerio.load(html);
-
   const statsTable = $('.table.table-bordered.table-hover.downloads > tbody > tr');
-  //loop through all table rows and get table data
-  statsTable.each(function() {
-    let title = $(this).find('td').text(); // get the text in all the td elements
-    let newStr = title.split("\t"); // convert text (string) into an array
-    newStr.shift(); // strip off empty array element at index 0
-    formatStr(newStr, dataObj); // format array string and store in an object
+  const data = [];
+
+  statsTable.each(function () {
+    let title = $(this).find('td').text();
+    let newStr = title.split("\t");
+    newStr.shift();
+    data.push(newStr);
   });
 
-  return dataObj;
-}
+  // Convert the data array to CSV format
+  const csvData = data.map(row => row.join(',')).join('\n');
 
-mainFunc().then((res) => {
-    // start worker
-    const worker = new Worker(workDir); 
-    console.log("Sending crawled data to dbWorker...");
-    // send formatted data to worker thread 
-    worker.postMessage(res);
-    // listen to message from worker thread
-    worker.on("message", (message) => {
-        console.log(message)
-    });
+  // Write the CSV data to a file
+  const filename = 'exchange-rates.csv';
+  const filepath = path.join(__dirname, filename);
+  fs.writeFileSync(filepath, csvData);
+
+  console.log(`Exchange rates data written to file: ${filename}`);
 });
 
-function formatStr(arr, dataObj){
-    // regex to match all the words before the first digit
-    let regExp = /[^A-Z]*(^\D+)/ 
-    let newArr = arr[0].split(regExp); // split array element 0 using the regExp rule
-    dataObj[newArr[1]] = newArr[2]; // store object 
+async function fetchData(url) {
+  console.log("Crawling data...")
+  let response = await axios(url).catch((err) => console.log(err));
+
+  if (response.status !== 200) {
+    console.log("Error occurred while fetching data");
+    return;
+  }
+  return response;
 }
