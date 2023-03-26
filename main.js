2890cb03b5ad6bd46df01cb4840732dabbfd4585
masilanzuki <99540869+masilanzuki@users.noreply.github.com>
2023-03-26 18:34:48 +0300

    comments added

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
+// Import necessary libraries
 const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
 const path = require('path');
 
+// Set the URL to crawl
 const url = "https://www.iban.com/exchange-rates";
 
+// Call the fetchData function and handle the response
 fetchData(url).then((res) => {
+  // Parse the HTML using Cheerio
   const html = res.data;
   const $ = cheerio.load(html);
+
+  // Find the table and extract the data
   const statsTable = $('.table.table-bordered.table-hover.downloads > tbody > tr');
   const data = [];
 
   statsTable.each(function () {
+    // Extract the table cell data and remove any unwanted characters
     let title = $(this).find('td').text();
     let newStr = title.split("\t");
     newStr.shift();
@@ -29,13 +36,19 @@ fetchData(url).then((res) => {
   console.log(`Exchange rates data written to file: ${filename}`);
 });
 
+// Define the fetchData function
 async function fetchData(url) {
   console.log("Crawling data...")
+
+  // Make an HTTP GET request to the specified URL using Axios
   let response = await axios(url).catch((err) => console.log(err));
 
+  // Handle any errors
   if (response.status !== 200) {
     console.log("Error occurred while fetching data");
     return;
   }
+
+  // Return the response
   return response;
 }
